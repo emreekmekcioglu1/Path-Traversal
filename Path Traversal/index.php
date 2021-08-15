3edd75b79de928332ec599272d3893c9d2fd6821
emreekmekcioglu1 <62750686+emreekmekcioglu1@users.noreply.github.com>
2021-08-15 23:33:48 +0300

    Add files via upload

---
+++ Path Traversal/index.php
@@ -0,0 +1,20 @@
+<?php 
+//To prevent possible Path Traversal attack uncomment lines(8-10-11) and comment line 19
+
+//$whitelist = ['welcome.html','sheldon.html','missy.html','georgie.html','marry.html','meemaw.html','john.html'];
+if (isset ($_GET ['page']))
+   {
+         $page = $_GET['page'];
+        //if(in_array($page, $whitelist)){
+          
+        //  echo file_get_contents($page);
+        //}  
+       
+   }
+   else
+        $page = 'welcome.html';
+         
+
+
+echo file_get_contents($page); //Comment this line
+?>
